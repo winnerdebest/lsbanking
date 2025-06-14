f40081d504cef6102e194c53ef2c262b16deaa04
Winner <winnerbrown9@gmail.com>
2025-06-13 23:29:58 -0700

    styling?

--- lsbanking/settings.py
+++ lsbanking/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 }
 
 
-POSTGRESS_LOCALLY = False
+POSTGRESS_LOCALLY = True
 if ENVIRONMENT == 'production' or POSTGRESS_LOCALLY == True:
         DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
