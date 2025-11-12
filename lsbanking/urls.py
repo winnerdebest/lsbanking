8a9cffbc794d904a7547d444c560c4a50659d3b5
winner <winnerbrown9@gmail.com>
2025-11-12 18:24:22 +0100

    This is rvrting it bak to th usual on

--- lsbanking/urls.py
+++ lsbanking/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')), 
     path('loans/', include('loans.urls')),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="text/plain")),
 
 
     # Path for investments allauth login and registration pages
